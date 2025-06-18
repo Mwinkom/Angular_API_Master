e1c42458b8ce128b015f996dacdf8ea62764bd62
Mildred Naab <mwinkomnaab@gmail.com>
2025-06-18 15:05:11 +0000

    Feat: Add routing, components, services, and guards for post management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/post-list/post-list.component')
+      .then(m => m.PostListComponent)
+  },
+  {
+    path: 'post/:id',
+    loadComponent: () => import('./components/post-detail/post-detail.component')
+      .then(m => m.PostDetailComponent)
+  },
+  {
+    path: 'create',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/post-form/post-form.component')
+      .then(m => m.PostFormComponent)
+  },
+  {
+    path: 'edit/:id',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/post-form/post-form.component')
+      .then(m => m.PostFormComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
