a8038d6cfbae9fe524d057a01e34d9c47a7e465d
Mildred Naab <mwinkomnaab@gmail.com>
2025-06-27 15:15:24 +0000

    feat: add create post form with validation

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.scss'
 })
-export class PostFormComponent {
+export class PostFormComponent implements OnInit {
+  postForm!: FormGroup;
+  isSubmitting = false;
+  successMessage = "";
+  errorMessage = "";
 
+  constructor(private router: Router, private fb: FormBuilder, private apiService: ApiService){}
+
+  ngOnInit(): void {
+    this.postForm = this.fb.group({
+      title : ['', [Validators.required, Validators.minLength(5)]],
+      body: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit(): void{
+    if(this.postForm.invalid) return;
+    this.isSubmitting = true;
+
+    const postData = this.postForm.value;
+
+    this.apiService.createPost(postData).subscribe({
+      next: () => {
+        this.router.navigate(['/'], { state: { successMessage: 'Post Created Successfully!' } });
+      },
+      error: (err) => {
+        this.errorMessage = err;
+        this.isSubmitting = false;
+      }
+    });
+  }
 }
--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
@@ -13,10 +13,18 @@ import { RouterLink } from '@angular/router';
 export class PostListComponent implements OnInit {
   posts: any[] = [];
   errorMessage: string = "";
+  successMessage: string = "";
 
-  constructor(private apiService: ApiService){}
+  constructor(private apiService: ApiService, private router: Router){}
 
   ngOnInit(): void {
+    // Check for success message from navigation state
+    if (history.state?.successMessage) {
+      this.successMessage = history.state.successMessage;
+      // Clear from history state to prevent reappearing on back navigation
+      history.replaceState({ ...history.state, successMessage: null }, '');
+      setTimeout(() => this.successMessage = '', 3000)}; 
+
     this.apiService.getPosts().subscribe({
       next: (data) => this.posts = data,
       error: (error) => this.errorMessage = error.message
